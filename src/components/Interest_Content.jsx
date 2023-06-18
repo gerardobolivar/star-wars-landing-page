06a1dcd39383ce7f3947ca4977a7bb4bf6ac8f81
asibajaedev <sibajaelizondoa@gmail.com>
2023-06-17 22:59:07 -0600

    Carousel functionality

--- src/components/Interest_Content.jsx
+++ src/components/Interest_Content.jsx
@@ -1,10 +1,11 @@
 import InterestContentSection from "./Interest_Content_Section";
 import './Interest_Content.css';
+import data from "./Interest_Content_Data";
 
 export default function Interest_Content() {
   return (
     <div className="interest-content">
-      <InterestContentSection />
+      <InterestContentSection data = {data.news}/>
     </div>
   );
 }
---
+++ src/components/Interest_Content_Data.jsx
@@ -0,0 +1,61 @@
+var data = {
+  news: [
+    {
+      title1: "From the Pages of Star Wars Insider: Behind the Scenes of The Bad Batch",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/star-wars-insider-219-article-feature_8a550828.jpeg?region=0%2C0%2C1600%2C900",
+      href1: "https://www.starwars.com/news/star-wars-insider-219-excerpt",
+      title2: "StarWars.com Fan Spotlight: Keith Yarde Is a Star Wars Fan Like His Father, Kerwin, Before Him",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/fan-spotlight-father-son-galaxy_840013ac.jpeg?region=0%2C0%2C1536%2C864",
+      href2: "https://www.starwars.com/news/fan-spotlight-father-son-galaxy",
+      title3: "Marvel’s Star Wars: The Mandalorian – Season 2 #1 Kicks Off with a Brawl — Exclusive Preview",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/marvel-the-mandalorian-season-2-issue-1_article-feature_5342792e.jpeg?region=0%2C0%2C1600%2C900",
+      href3: "https://www.starwars.com/news/marvel-the-mandalorian-season-2-issue-1",
+    },
+    {
+      title1: "Meet the Visionaries: Paul Young on the Irish Folklore-Inspired Tale of “Screecher’s Reach”",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/visions-screechers-reach-interview-sashdouble-feature_b70243fd.jpeg?region=0%2C0%2C1280%2C720",
+      href1: "https://www.starwars.com/news/star-wars-visions-screechers-reach-interview",
+      title2: "Star Wars Outlaws Revealed: Get the First Details on Ubisoft’s Open-World Star Wars Game - Updated",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art_article-feature_73a536a1.jpeg?region=0%2C0%2C1600%2C900",
+      href2: "https://www.starwars.com/news/star-wars-outlaws",
+      title3: "Visions Revisited: 5 Highlights from “Aau’s Song”",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/visions-highlights-aaus-song_article-feature_b0361b82.jpeg?region=0%2C0%2C1600%2C900",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-aaus-song",
+    },
+    {
+      title1: "Meet the Visionaries: LeAndre Thomas and Justin Ridge on the Heartbreak and Hope of “The Pit”",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/sw-visions-the-pit-interview-sashdouble-feature_03024717.jpeg?region=0%2C0%2C1280%2C720",
+      href1: "https://www.starwars.com/news/star-wars-visions-the-pit-interview",
+      title2: "10 Great LEGO Star Wars Building Sets for Adults",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/10-lego-star-war-sets_article-feature_8454e219.jpeg?region=0%2C0%2C1600%2C900",
+      href2: "https://www.starwars.com/news/lego-star-wars-for-adults",
+      title3: "Visions Revisited: 5 Highlights from “The Pit”",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/sw-visions-highlights-the-pit_article-feature_681e4eab.jpeg?region=0%2C0%2C1600%2C900",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-the-pit",
+    },
+    {
+      title1: "Visions Revisited: 5 Highlights from “The Bandits of Golak”",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/the-bandits-of-golak-highlights_article-feature_26314410.jpeg?region=0%2C0%2C1600%2C900",
+      href1: "https://www.starwars.com/news/star-wars-visions-highlights-the-bandits-of-golak",
+      title2: 'These Max Rebo Donuts are the Biggest Hit Since "Lapti Nek"',
+      src2: "https://lumiere-a.akamaihd.net/v1/images/max-rebo-donuts-article-feature_f10edf0f.jpeg?region=0%2C0%2C1600%2C900",
+      href2: "https://www.starwars.com/news/max-rebo-donuts",
+      title3: "Wheel of Fortune Spins to the Galaxy Far, Far Away",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/galactic-celebration-wheel-of-fortune-featured_b4e56005.jpeg?region=0%2C0%2C1536%2C864",
+      href3: "https://www.starwars.com/news/wheel-of-fortune-star-wars",
+    },
+    {
+      title1: "D-Squad Is Back! R2-D2’s All-Star Team Returns in New Marvel Miniseries",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/star-wars-dark-droids-d-squad-cover_article-feature_f447da56.jpeg?region=0%2C0%2C1600%2C900",
+      href1: "https://www.starwars.com/news/marvel-dark-droids-d-squad",
+      title2: "The Empire Lands on Endor in Marvel’s Star Wars: Return of the Jedi - The Empire #1 — Exclusive Preview",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/marvel-sw-rotj-the-empire-1-preview_article-feature_0dedeaad.jpeg?region=0%2C0%2C1600%2C900",
+      href2: "https://www.starwars.com/news/marvel-star-wars-return-of-the-jedi-the-empire-1-preview",
+      title3: "Visions Revisited: 5 Highlights from “The Spy Dancer”",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/sw-visions-highlights-the-spy-dancer_article-feature_770a927f.jpeg?region=0%2C0%2C1600%2C900",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-spy-dancer",
+    }
+  ]
+}
+
+export default data;
