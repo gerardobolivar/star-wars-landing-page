a25aea1d3df6f407e2a43be171c1a7c1968d427c
asibajaedev <sibajaelizondoa@gmail.com>
2023-06-17 23:03:51 -0600

    Workspace cleaning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './App.css';
-import Cataloge from "./components/cataloge.js";
 import InterestContent from "./components/Interest_Content";
 
 function App() {
