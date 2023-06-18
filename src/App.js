51611a929ac266b08fe3bcf87511de743283feb1
Isaac Abarca <abarcai532@gmail.com>
2023-06-18 13:15:19 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './App.css';
 import Cataloge from "./components/cataloge.js";
+import Footer from "./components/Footer.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <div className="App">
       <Cataloge/>
+      <Footer/>
     </div>
   );
 }
