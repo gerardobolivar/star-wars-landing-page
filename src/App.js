54144635c5b69efafdab958adbdfac65f66c87c5
GERARDO BOLIVAR FERNANDEZ MENA <gerardo.fernandezmena@ucr.ac.cr>
2023-06-20 15:58:07 -0600

    Monir fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import './App.css';
+import Separador from "./componentes/Separador.js";
+import Navbar from "./componentes/Navbar.js";
+import Carousel from "./componentes/Carousel.js";
 import InterestContent from "./components/Interest_Content";
 import Cataloge from "./components/cataloge.js";
 import Footer from "./components/Footer.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <InterestContent/>
-      <Cataloge/>
-      <Footer/>
+      <Navbar />
+      <Carousel/>
+      <Separador/>
+      <InterestContent />
+      <Cataloge />
+      <Footer />
     </div>
   );
 }
