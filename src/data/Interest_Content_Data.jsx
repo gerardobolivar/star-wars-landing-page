a6318a161b8be95e30e6583a5861aa86907b2c4f
Jose Armando Sibaja Elizondo <sibajaelizondoa@gmail.com>
2023-06-28 12:24:38 -0600

    Minor fixes

--- src/data/Interest_Content_Data.jsx
+++ src/data/Interest_Content_Data.jsx
@@ -9,7 +9,7 @@ var data = {
       href2: "https://www.starwars.com/news/fan-spotlight-father-son-galaxy",
       title3: "Marvel’s Star Wars: The Mandalorian – Season 2 #1 Kicks Off with a Brawl — Exclusive Preview",
       src3: "https://lumiere-a.akamaihd.net/v1/images/marvel-the-mandalorian-season-2-issue-1_article-feature_5342792e.jpeg?region=0%2C0%2C1600%2C900",
-      href3: "https://www.starwars.com/news/marvel-the-mandalorian-season-2-issue-1",
+      href3: "https://www.starwars.com/news/marvel-the-mandalorian-season-2-issue-1"
     },
     {
       title1: "Meet the Visionaries: Paul Young on the Irish Folklore-Inspired Tale of “Screecher’s Reach”",
@@ -20,7 +20,7 @@ var data = {
       href2: "https://www.starwars.com/news/star-wars-outlaws",
       title3: "Visions Revisited: 5 Highlights from “Aau’s Song”",
       src3: "https://lumiere-a.akamaihd.net/v1/images/visions-highlights-aaus-song_article-feature_b0361b82.jpeg?region=0%2C0%2C1600%2C900",
-      href3: "https://www.starwars.com/news/star-wars-visions-highlights-aaus-song",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-aaus-song"
     },
     {
       title1: "Meet the Visionaries: LeAndre Thomas and Justin Ridge on the Heartbreak and Hope of “The Pit”",
@@ -31,7 +31,7 @@ var data = {
       href2: "https://www.starwars.com/news/lego-star-wars-for-adults",
       title3: "Visions Revisited: 5 Highlights from “The Pit”",
       src3: "https://lumiere-a.akamaihd.net/v1/images/sw-visions-highlights-the-pit_article-feature_681e4eab.jpeg?region=0%2C0%2C1600%2C900",
-      href3: "https://www.starwars.com/news/star-wars-visions-highlights-the-pit",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-the-pit"
     },
     {
       title1: "Visions Revisited: 5 Highlights from “The Bandits of Golak”",
@@ -42,7 +42,7 @@ var data = {
       href2: "https://www.starwars.com/news/max-rebo-donuts",
       title3: "Wheel of Fortune Spins to the Galaxy Far, Far Away",
       src3: "https://lumiere-a.akamaihd.net/v1/images/galactic-celebration-wheel-of-fortune-featured_b4e56005.jpeg?region=0%2C0%2C1536%2C864",
-      href3: "https://www.starwars.com/news/wheel-of-fortune-star-wars",
+      href3: "https://www.starwars.com/news/wheel-of-fortune-star-wars"
     },
     {
       title1: "D-Squad Is Back! R2-D2’s All-Star Team Returns in New Marvel Miniseries",
@@ -53,7 +53,68 @@ var data = {
       href2: "https://www.starwars.com/news/marvel-star-wars-return-of-the-jedi-the-empire-1-preview",
       title3: "Visions Revisited: 5 Highlights from “The Spy Dancer”",
       src3: "https://lumiere-a.akamaihd.net/v1/images/sw-visions-highlights-the-spy-dancer_article-feature_770a927f.jpeg?region=0%2C0%2C1600%2C900",
-      href3: "https://www.starwars.com/news/star-wars-visions-highlights-spy-dancer",
+      href3: "https://www.starwars.com/news/star-wars-visions-highlights-spy-dancer"
+    }
+  ],
+  latestVideo: [
+    {
+      title1: "Indiana Jones is Back in Action, John Williams Performs, and More!",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/image_a9bcb52f.jpeg?region=0,0,1920,1080&amp;width=600",
+      href1: "https://www.starwars.com/video/indiana-jones-is-back-in-action-john-williams-performs-and-more",
+      time1: "5:17",
+      title2: "Official Gameplay Walkthrough | Star Wars Outlaw",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/kay_nd5_wide_thumbnail_7ac38cfb.jpeg?region=0,0,1920,1080&amp;width=600",
+      href2: "https://www.starwars.com/video/official-gameplay-walkthrough-star-wars-outlaw",
+      time2: "10:38",
+      title3: "World Premiere Trailer | Star Wars Outlaws",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/image_9c63d62b.jpeg?region=0,0,1920,1080&amp;width=600",
+      href3: "https://www.starwars.com/video/world-premiere-trailer-star-wars-outlaws",
+      time3: "2:26"
+    },
+    {
+      title1: "Ahsoka Release Date Announced, Hayden Christensen and Diego Luna Chat, and More!",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/image_0abee5f2.jpeg?region=0,0,1920,1080&amp;width=480",
+      href1: "https://www.starwars.com/video/ahsoka-release-date-announced-hayden-christensen-and-diego-luna-chat-and-more",
+      time1: "5:36",
+      title2: '"Begin" | Ahsoka',
+      src2: "https://lumiere-a.akamaihd.net/v1/images/image_dab64477.jpeg?region=0,0,1920,1080&amp;width=480",
+      href2: "https://www.starwars.com/video/begin-ahsoka",
+      time2: "0:30",
+      title3: "Celebrating 40 Years of Return of the Jedi",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/image_d5f9da56.jpeg?region=0,0,1920,1080&amp;width=480",
+      href3: "https://www.starwars.com/video/celebrating-40-years-of-return-of-the-jedi",
+      time3: "3:05"
+    },
+    {
+      title1: "Rise of the Red Blade Reveal, The Mandalorian: The Manga First Look, and More!",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/image_a6262f82.jpeg?region=0,0,1920,1080&amp;width=480",
+      href1: "https://www.starwars.com/video/rise-of-the-red-blade-reveal-the-mandalorian-the-manga-first-look-and-more",
+      time1: "4:34",
+      title2: "Celebrating Carrie Frances Fisher on the Walk of Fame",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/image_ae989077.jpeg?region=0,0,1920,1080&amp;width=480",
+      href2: "https://www.starwars.com/video/celebrating-carrie-frances-fisher-on-the-walk-of-fame",
+      time2: "3:10"
+    }
+  ],
+  exploreMore: [
+    {
+      title1: "",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/bobf-franchise-slider_efe95678.jpeg?region=0,0,1536,864&width=600",
+      href1: "https://www.starwars.com/disneyplus",
+      title2: "",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/thr-franchise-slider_6d73dcd2.jpeg?region=0,0,800,420&width=600",
+      href2: "https://www.starwars.com/the-high-republic",
+      title3: "",
+      src3: "https://lumiere-a.akamaihd.net/v1/images/franchise-slider-swce23_5c8c2aed.jpeg?region=0,0,1536,864&width=600",
+      href3: "https://www.starwars.com/star-wars-celebration"
+    },
+    {
+      title1: "",
+      src1: "https://lumiere-a.akamaihd.net/v1/images/bottom-slider-logos_5d5563d4.jpeg?region=0,0,1536,864&width=600",
+      href1: "https://www.starwars.com/interactive/virtual-reality",
+      title2: "",
+      src2: "https://lumiere-a.akamaihd.net/v1/images/ffc-bottom-slider_6666c49b.jpeg?region=0,0,1536,864&width=600",
+      href2: "https://www.starwars.com/force-for-change"
     }
   ]
 }
